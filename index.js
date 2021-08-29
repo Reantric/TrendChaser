93012cf57a0464a2128b8ea8e86d25d2fdfd20f2
John <44213272+protonlaser91@users.noreply.github.com>
2021-08-28 23:34:42 -0400

    redirect to ailun

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const express = require('express')
 const app = express()
 
+const map = {
+  12: "24 hours",
+  13: "7 days",
+  14: "1 month",
+  15: "3 months",
+  16: "6 months"
+};
 
 const port = 3000
 
@@ -24,19 +31,14 @@ app.listen(port, () => {
   const body = req.body;
   let place = body.region.split(',');
   if (place.length < 2){
-    //alert('Please enter a valid region')
     res.redirect('/')
     return;
   }
-
+  const state = place[place.length-2]
+  if (place.length > 2) 
+    var city = place[place.length-3]
+  console.log(state,city,map[body.timeframe]);
   res.send("Hello WOrld");
 }) 
 
 
-exports.map = {
-  12: "24 hours",
-  13: "7 days",
-  14: "1 month",
-  15: "3 months",
-  16: "6 months"
-};
---
+++ trends.js
@@ -0,0 +1,28 @@
+const googleTrends = require('google-trends-api');
+const fs = require('fs')
+//let state = ;
+//let zipcode = ;
+let location = "US-NJ-501";
+let lastHours = 24;
+let offset = new Date().getTimezoneOffset();
+let timenow = new Date();
+let timethen = new Date();
+timethen.setHours(timethen.getHours() - lastHours);
+googleTrends.relatedQueries({keyword: 'Chinese Food', startTime: timethen, endTime: timenow, geo: location, timezone: offset, granularTimeResolution: true})
+.then(function(results) {
+  console.log(results);
+})
+.catch(function(err) {
+  console.error(err);
+})
+googleTrends.relatedTopics({keyword: 'Chinese Food', startTime: timethen, endTime: timenow, geo: location, timezone: offset, granularTimeResolution: true})
+.then(function(results) {
+  console.log(results);
+  fs.writeFile("stuff.json",results, function (err) {
+    if (err) return console.log(err);
+  });
+})
+.catch(function(err) {
+  console.error(err);
+})
+
