bb6f83580733261da15fcd9d1ff70b09ffdd0682
Eric Zou <55918461+St0nkyCheeseMan@users.noreply.github.com>
2021-08-29 11:02:15 -0400

    fixing json

--- index.js
+++ index.js
@@ -44,28 +44,31 @@ app.post('/results', (req,res) => {
   timethen = new Date();
   timethen.setDate(timethen.getDate()-daysBack);
   const googleTrends = require('google-trends-api');
-  var relQ, relT;
   googleTrends.relatedTopics({keyword: keyworde, startTime: timethen, endTime: timenow, geo: dma, granularTimeResolution: true})
-    .then((res) => {
-      relT = res;
+.then((res) => {
+  fs.writeFile('public/relatedTopics.json',res,function(err) {
+    if (err) throw err;
+    console.log('complete');
     })
-    .catch((err) => {
-      console.log(err);
-    })
-
+})
+.catch((err) => {
+  console.log(err);
+})
   googleTrends.relatedQueries({keyword: keyworde, startTime: timethen, endTime: timenow, geo: dma, granularTimeResolution: true})
-    .then((res) => {
-      relT = res;
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  
+  .then((res) => {
+    fs.writeFile('public/relatedQueries.json',res,function(err) {
+      if (err) throw err;
+      console.log('complete');
+      })
+  })
+  .catch((err) => {
+    console.log(err);
+  })
   //problem here: results page uses old json data each time 
   //(might be because it takes too long to write to JSON files)
   //
   
-  res.render("results", {relatedQueries:relQ, relatedTopics:relT});
+  res.render("results");
 }) 
 
 
