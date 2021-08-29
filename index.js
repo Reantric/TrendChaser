0f49396da1341d935c5d988abe242b61eda06d97
John <44213272+protonlaser91@users.noreply.github.com>
2021-08-29 10:21:53 -0400

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
 const express = require('express')
 const app = express()
 
 const map = {
-  12: "24 hours",
-  13: "7 days",
-  14: "1 month",
-  15: "3 months",
-  16: "6 months"
+  12: 1,
+  13: 7,
+  14: 31,
+  15: 93,
+  16: 186
 };
 
 const port = 3000
@@ -16,8 +15,7 @@ app.use(express.json());
 
 app.set('view engine', 'jade');
 app.use(express.static('public'))
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
+app.use(express.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -27,23 +25,46 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
- app.post('/ailun', (req,res) => {
+app.post('/results', (req,res) => {
+  const fs = require('fs')
   const body = req.body;
-  let place = body.region.split(',');
+  let place = body.region.split(', ');
+  let zipcode;
   if (place.length < 2){
-    res.redirect('/')
-    return;
-  }
-  const state = place[place.length-2]
-  if (place.length > 2) 
-    var city = place[place.length-3]
-  console.log(state,city,map[body.timeframe]);
-  res.render("ailun")
-  // res.end();
+     zipcode = place[0]
+  } else
+    zipcode = place[place.length-2].slice(-5);
+
+  const keyworde = body.business;
+  const daysBack = map[body.timeframe];
+  let rawdata = fs.readFileSync('public/dmazip.json');
+  let student = JSON.parse(rawdata);
+  const dma = student[zipcode];
+  timenow = new Date();
+  timethen = new Date();
+  timethen.setDate(timethen.getDate()-daysBack);
+  console.log(dma);
+  const googleTrends = require('google-trends-api');
+  var relQ, relT;
+  googleTrends.relatedTopics({keyword: keyworde, startTime: timethen, endTime: timenow, geo: dma, granularTimeResolution: true})
+    .then((res) => {
+      relT = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+  googleTrends.relatedQueries({keyword: keyworde, startTime: timethen, endTime: timenow, geo: dma, granularTimeResolution: true})
+    .then((res) => {
+      relT = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  
+  //problem here: results page uses old json data each time 
+  //(might be because it takes too long to write to JSON files)
+  //
+  
+  res.render("results", {relatedQueries:relQ, relatedTopics:relT});
 }) 
-
-app.get("/ailun", (req,res) => {
-  res.render("ailun")
-})
-
-
